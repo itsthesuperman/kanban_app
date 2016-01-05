9e36b19d6e81a229e2081aeb34752d9b21c6224f
wrights <wrights@teds.com>
2016-01-05 12:52:07 -0500

    Added a notes component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
-import Note from './Note.jsx';
+import React, { Component } from 'react';
+import uuid from 'node-uuid';
 
-export default class App extends React.Component {
-  render() {
-    return <Note />;
+import Note from './Note.jsx';
+import Notes from './Notes'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do other things'
+        }
+      ]
+    };
   }
+
+  render() {
+
+   const notes = this.state.notes;
+
+   return (
+     <div>
+
+       <button className="add-note" onClick={this.addNote}>+</button>
+       <Notes notes={notes} />
+     </div>
+   );
+ }
+
+ addNote = () => {
+   this.setState({
+     notes: this.state.notes.concat([{
+       id: uuid.v4(),
+       task: 'New Task'
+     }])
+   });
+ }
 }
