a156084f2c61fc9d94c7dac015d822dd284f8f26
wrights <wrights@teds.com>
2016-01-05 11:32:54 -0500

    Initial commit for kanban app

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: PATHS.app
+      }
+    ],
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'Kanban app',
+      appMountId: 'app'
+    })
+  ]
+};
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
