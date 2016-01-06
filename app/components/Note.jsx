0756347678d677c4515662bce60f52247698d9c4
wrights <wrights@teds.com>
2016-01-06 08:10:51 -0500

    Began adding an editable note functionality to the component

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default ({task}) => <h2>{task}</h2>;
+export default class Note extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text"
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  }
+  renderNote = () => {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  }
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  }
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+  finishEdit = (e) => {
+    // A smarter way to deal with this would be to set `defaultProps`.
+    // See *Typing with React* chapter for more information.
+    if(this.props.onEdit) {
+      this.props.onEdit(e.target.value);
+    }
+
+    this.setState({
+      editing: false
+    });
+  }
+}
